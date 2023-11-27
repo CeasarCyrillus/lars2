874a49e2c6b9cf6044ef7204406d5eed79122573
ceasarcyrillus <ceasar.cyrillus@protonmail.com>
2023-11-27 20:21:39 +0100

    Add language selector

--- src/features/main/Header.tsx
+++ src/features/main/Header.tsx
@@ -2,12 +2,14 @@ import {HeaderStyled} from "./Header.style";
 import {LogoIcon} from "./LogoIcon";
 import {Navigation} from "./Navigation";
 import {LogoIconStyled} from "./LogoIcon.style";
+import {LanguageSelector} from "../languageSelect/LanguageSelector";
 
 export const Header = () => {
   return <HeaderStyled>
     <LogoIconStyled>
       <LogoIcon/>
     </LogoIconStyled>
+    <LanguageSelector/>
     <Navigation/>
   </HeaderStyled>
 }
