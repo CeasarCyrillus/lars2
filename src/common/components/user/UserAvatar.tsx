b07a696b1909321c93721965100c82432abfd71b
ceasarcyrillus <ceasar.cyrillus@protonmail.com>
2024-02-25 17:15:24 +0100

    Loading animation for user avatar

--- src/common/components/user/UserAvatar.tsx
+++ src/common/components/user/UserAvatar.tsx
@@ -1,12 +1,20 @@
-import {Avatar} from "@mui/material";
-import React from "react";
+import {Avatar, Box, Skeleton} from "@mui/material";
+import {useState} from "react";
 
 type Props = {
   name: string,
   width?: number,
   height?: number
 }
-export const UserAvatar = ({name, height, width}: Props) => <Avatar
-  src={`https://ui-avatars.com/api/?background=random&name=${name}`}
-  sx={{width: width ?? 30, height: height ?? 30}}
-/>
+export const UserAvatar = ({name, height, width}: Props) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const dimensions = {width: width ?? 30, height: height ?? 30};
+  return <Box {...dimensions}>
+    <Avatar
+      src={`https://ui-avatars.com/api/?background=random&name=${name}`}
+      sx={{...dimensions, display: isLoaded ? "block" : "none"}}
+      onLoad={() => setIsLoaded(true)}
+    />
+    {!isLoaded && <Skeleton variant={"circular"} {...dimensions}/>}
+  </Box>
+}
